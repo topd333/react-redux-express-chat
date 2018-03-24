ebf4d6d7ef8124fb6669fe9b35a65995bafc5af3
topd333 <genius.snow0405@gmail.com>
2018-03-24 14:36:55 +0100

    update status code

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -24,12 +24,12 @@ exports.login = function (req, res, next) {
     if(user){
       const userInfo = setUserInfo(user);
 
-      res.status(200).json({
+      return res.status(201).json({
         token: `JWT ${generateToken(userInfo)}`,
         user: userInfo
       });
     } else {
-      res.status(401).send(info);
+      return res.status(422).send(info);
     }
   })(req, res);
 };
